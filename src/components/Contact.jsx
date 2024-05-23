1fbcbce43367f0dbc85e131d37c96d69c186cc08
Alok31796 <alok31796@gmail.com>
2024-05-23 19:58:56 +0530

    I did some test cases

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,24 @@
 const Contact = () => {
-  return <h2>Contact</h2>;
+  return (
+    <div>
+      <h1 className="font-bold text-3xl p-4 m-4">Contact Page</h1>
+      <form>
+        <input
+          type="text"
+          className="border border-black m-2 p-2"
+          placeholder="name"
+        />
+        <input
+          type="text"
+          className="border border-black m-2 p-2"
+          placeholder="message"
+        />
+        <button className=" m-2 p-2 text-white bg-red-500 rounded-lg">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Contact;
---
+++ src/components/__tests__/contact.test.js
@@ -0,0 +1,33 @@
+import { render, screen } from "@testing-library/react";
+import Contact from "../Contact";
+import "@testing-library/jest-dom";
+
+test("Should load contact us component", () => {
+  render(<Contact />);
+  const heading = screen.getByRole("heading");
+  expect(heading).toBeInTheDocument();
+});
+
+test("Should load button inside contact us component", () => {
+  render(<Contact />);
+  const button = screen.getByRole("button");
+  expect(button).toBeInTheDocument();
+});
+
+test("Should load input name inside contact us component", () => {
+  render(<Contact />);
+  const input = screen.getByPlaceholderText("name");
+  expect(input).toBeInTheDocument();
+});
+
+test("Should load 2 input boxes inside contact us component", () => {
+  render(<Contact />);
+  // Querying
+  const inputBoxes = screen.getAllByRole("textbox");
+
+  // return react jsx
+  console.log(inputBoxes.length);
+  // Asserton
+  // expect(inputBoxes.length).not.toBe(3);  //you can write this also
+  expect(inputBoxes.length).toBe(2);
+});
